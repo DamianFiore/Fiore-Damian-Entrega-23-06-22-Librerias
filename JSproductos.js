4756033a296faa13f2bfd23fae8d7deae0308684
Damián Fiore <d.eze.fiore@gmail.com>
2022-06-23 22:24:02 -0300

    librerias

---
+++ JSproductos.js
@@ -0,0 +1,202 @@
+class Producto{
+    constructor(imagen,nombre,precio,descripcion){
+        this.imagen=imagen;
+        this.nombre=nombre;
+        this.precio=precio;
+        this.descripcion=descripcion;
+    }
+}
+
+const producto1=new Producto(`./Imagenes/Etiopia.jpg`,`ETIOPIA`,1850,`Un café muy dulce, con dejo de sabor a cítrico y floral. Acidez vivaz, un cuerpo sedoso y una complejidad de notas florales en el aroma, a veces con un toque de coco tostado.`);
+
+const producto2=new Producto(`./Imagenes/Costa Rica.jpg`,`COSTA RICA`,1500,`De gran cuerpo y aroma, se asienta en boca, con notas achocolatadas hasta un rango de mayor complejidad, donde los buenos degustadores podrán encontrar sabores a los cítricos como naranja, además a melocotón, miel de abeja y vainilla, etc.`);
+
+const producto3=new Producto(`./Imagenes/Vainilla.jpg`,`VAINILLA`,1250,`Realizados con el grano 100% arabigo y extractos naturales hasta un 3%. Estos extractos se agregan durante el proceso de tostado, permitiendo deslizar un sutil sabor sin opacar la fuerza del cafe. Blend de granos 100 % arábigos.`);
+
+const producto4=new Producto(`./Imagenes/Chocolate.jpg`,`CHOCOLATE`,1250,`Realizados con el grano 100% arabigo y extractos naturales hasta un 3%. Estos extractos se agregan durante el proceso de tostado, permitiendo deslizar un sutil sabor sin opacar la fuerza del cafe.`);
+
+const producto5=new Producto(`./Imagenes/Congo.jpg`,`CONGO`,1850,`Elegante y brillante acidez de arándano, azúcar de caña y notas de ciruela con cuerpo almibarado y buen equilibrio.`);
+
+const producto6=new Producto(`./Imagenes/Colombian Decaff.jpg`,`COLOMBIAN DECAFF`,1250,`Aroma muy fragante, con mucha dulzura. Tratado para remover la cafeína permitiendo al café, conservar su cuerpo aroma y sabor.`);
+
+const producto7=new Producto(`./Imagenes/Colombian Dark.jpg`,`COLOMBIAN DARK`,1250,`Un café muy dulce, con dejo de sabor a cítrico y floral. Acidez vivaz, un cuerpo sedoso y una complejidad de notas florales en el aroma. Nuestro Colombian Supremo sometido al máximo tueste intensificando aroma, sabor y reduciendo acidez.`);
+
+const producto8=new Producto(`./Imagenes/Brasil Santos Bourdon.jpg`,`BRASIL SANTOS BOURDON`,1150,`Blend de granos 100 % arábigos de Santos Bourbon en diferentes etapas, ciclos. El resultado un café más fino y equilibrado. Intensidad: Suave. Sofisticada selección de granos que conforman un blend suave y cremoso con un dejo levemente dulce.`);
+
+const producto9=new Producto(`./Imagenes/Brasil Medium Roast.jpg`,`BRASIL MEDIUM ROAST`,1150,`Un café muy dulce, con dejo de sabor a cítrico y floral. Acidez vivaz, un cuerpo sedoso y una complejidad de notas florales en el aroma. Nuestro Brasil Santos Bourbon con un tueste más intenso que potencia sus cualidades.`);
+
+const producto10=new Producto(`./Imagenes/Papua Nueva Guinea.jpg`,`PAPÚA NUEVA GUINEA`,1950,`Esta región disfruta de un clima fresco, templado y con precipitaciones todo el año, lo que genera condiciones micro climáticas ideales para su cultivo. Sabor audaz, notas de chocolate, dulce, ácido y afrutado con un cuerpo medio a fuerte y un final ligeramente cremoso y suave.`);
+
+const producto11=new Producto(`./Imagenes/Black Tea.jpg`,`Black Tea`,600,`El té negro es un té que se encuentra oxidado en mayor grado que otras variedades, con un aroma intenso y la justa dosis de amargura.`);
+
+const producto12=new Producto(`./Imagenes/Fire Tea.jpg`,`Fire Tea`,750,`La infusión Fire Tea es té rojo, hierbas y flores que estimulan la circulación sanguínea y actúan como afrodisíaco natural.`);
+
+const producto13=new Producto(`./Imagenes/Goog Night Tea.jpg`,`Good Night Tea`,750,`Good Night es un té de hierbas con la adición de adaptógenos, mejorando la calidad del sueño y relajando el cuerpo y la mente. El ritual perfecto para personas con exceso de trabajo.`);
+
+const producto14=new Producto(`./Imagenes/Immune support Tea.jpg`,`Immune Support`,800,`Calentamiento de frutas y té de hierbas con la adición de adaptógenos y especias, lo que naturalmente fortalece el sistema inmunológico.`);
+
+const producto15=new Producto(`./Imagenes/Inner Peace  Tea.jpg`,`Inner Peace Tea`,750,`El té de hierbas  Inner Peace es como un buen amigo: choca los cinco en todos esos hermosos momentos en los que simplemente se deja llevar. La combinación de hierbas, adaptógenos y hongos Reishi te permite encontrar paz interior y alivio.`);
+
+const producto16=new Producto(`./Imagenes/Pure Beauty Tea.jpg`,`Pure Beauty Tea`,650,`Nuestro té suelto con la adición de adaptógenos es el mismo bueno y una parte integral del ritual de embellecimiento diario.`);
+
+const producto17=new Producto(`./Imagenes/Brain Booster Tea.jpg`,`Brain Booster Tea`,850,`Brain Booster es gravedad para tus pensamientos. El té suelto combina hierbas y adaptógenos que aumentan la concentración a corto plazo y apoyan el trabajo del cerebro. La infusión te da energía y reduce el impacto del estrés en tu mente.`);
+
+const producto18=new Producto(`./Imagenes/Chocolates/Bittersweet Dark Chocolate.jpg`,`Bittersweet Dark Chocolate`,450,`Alto porcentaje de cacao y un mínimo de azúcar, se trata de explorar la personalidad y el sabor de los granos.`);
+
+const producto19=new Producto(`./Imagenes/Chocolates/Chilli and cinnamon Dark Chocolate.jpg`,`Chilli and cinnamon Dark Chocolate`,450,`Equilibrio perfecto de canela suave y hojuelas de chile rojo ardiente con chocolate amargo al 70% ligeramente dulce. Obtenemos chiles orgánicos y canela localmente.`);
+
+const producto20=new Producto(`./Imagenes/Chocolates/Coconut Milk Dark Chocolate.jpg`,`Coconut Milk Dark Chocolate`,450,`Una barra de chocolate con leche vegana suave y cremosa hecha con deliciosa leche de coco. Esta barra no contiene lácteos y es indulgente.`);
+
+const producto21=new Producto(`./Imagenes/Chocolates/Crunchy Peanut Butter Dark Chocolate.jpg`,`Crunchy Peanut Butter Dark Chocolate`,450,`Molimos mantequilla de maní orgánica en nuestro molinillo de piedra junto con el chocolate para obtener una barra suave como la seda, agregamos maní entero y trocitos de cacao crujientes para más textura.`);
+
+const producto22=new Producto(`./Imagenes/Chocolates/Espresso Dark Chocolate.jpg`,`Espresso Dark Chocolate`,450,`Equilibrio perfecto entre el café orgánico de una sola finca y nuestro chocolate semidulce. El café y el cacao fueron elegidos por su sinergia, creando una armonía entre los sabores.`);
+
+const producto23=new Producto(`./Imagenes/Chocolates/Rosemary & Sea Salt Dark Chocolate.jpg`,`Rosemary & Sea Salt Dark Chocolate`,450,`El sabor salado de la sal marina combinado con el sabor profundo y seductor del romero realza las notas dulces y ricas de chocolate del 70 % de chocolate amargo.`);
+
+const producto24=new Producto(`./Imagenes/Chocolates/Sea Salt Dark Chocolate.jpg`,`Semi-Sweet Dark Chocolate`,450,`Esta barra es una versión ligeramente más dulce de nuestro exclusivo chocolate amargo, una suave introducción al mundo del chocolate amargo.`);
+
+const producto25=new Producto(`./Imagenes/Chocolates/Zesty Orange Dark Chocolate.jpg`,`Zesty Orange Dark Chocolate`,450,`Exclusivo chocolate negro agridulce al 75 % y lo hemos combinado con naranja amarga y picante para crear una barra fresca y afrutada.`);
+
+const Allproducts = [producto1, producto2, producto3, producto4,producto5, producto6, producto7, producto8,producto9, producto10, producto11, producto12,producto13, producto14, producto15, producto16,producto17, producto18, producto19, producto20,producto21, producto22, producto23, producto24, producto25];
+
+let carrito=[];
+
+const mainproductos = document.querySelector(`#main-p`); 
+
+Allproducts.forEach((producto)=>{
+    const prod = document.createElement(`div`);
+    prod.className=`prod`;
+    prod.innerHTML=`
+        <img src="${producto.imagen}" alt="etiopia">
+        <name>${producto.nombre}</name>
+        <des>${producto.descripcion}</des>
+        <price>$${producto.precio}</price>
+        <div class="boton-p">
+            <button id="${producto.nombre}" type="button" class="comp btn btn-dark" href="productos.html">COMPRAR</button>
+        </div>
+        `
+    mainproductos.append(prod);
+})
+
+const AgregarProducto = (e) =>{
+    const productoElegido = e.target.getAttribute(`id`);
+    const producto = Allproducts.find((producto) => producto.nombre == productoElegido);
+    carrito.push(producto);
+    console.log(carrito);
+    const numero = document.querySelector(`.productoscarrito`);
+    numero.innerHTML=`${carrito.length}`
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Producto agregado al carrito',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
+const botonesCompra = document.querySelectorAll('.comp')
+botonesCompra.forEach((botonCompra) => {
+    botonCompra.addEventListener('click',AgregarProducto)
+})
+
+function confirmarcompra(){
+    Swal.fire({
+        title: '¿Seguro que quiere confirmar la compra?',
+        text: "No podrá cancelarla una vez que acepte",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Aceptar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Compra exitosa',
+            'Muchas Gracias',
+            'success'
+          )
+        }
+      })
+}
+
+const botonconfirmacion = document.querySelectorAll('.confirm')
+botonconfirmacion.forEach((botonconfirmacion) => {
+    botonconfirmacion.addEventListener('click',confirmarcompra)
+})
+
+
+
+/*    (ESTA FUNCIÓNN LA PONDRÉ CUANDO AGREGUE EL CARRITO DE COMPRA CON LA IMAGEN, EL CONTADOR Y LA LISTA DE PRODUCTOS)
+function EliminarProducto()
+{
+    let item= prompt("Indique que número de la lista desea eliminar")
+    carrito.splice(item);
+}
+*/
+
+
+
+/* (ESTO TAMBIÉN LO COMENTO POR QUE IRIA CON LA PARTE DE EL PAGO QUE TODAVÍA NO DESARROLLO EN HTML Y CSS)
+let total=0;
+for (let i=0; i=10; i++){ 
+    let respuesta= prompt("¿Quiere agregar un producto al carrito? (si/no)");
+    
+    if(respuesta=="si"){
+        AgregarProducto();
+        total= total + parseInt(this.precio);
+        console.log(carrito);
+        alert("El total de su compra es: $" + parseInt(total));
+    }
+    else{
+        break;
+    }
+}
+
+let resp=prompt("¿Quiere eliminar algun producto del carrito? (si/no)");
+if(resp=="si"){
+    EliminarProducto();
+    console.log(carrito);
+    alert("Se elimino del carrito:" + this.tipo);
+}
+
+let respueta2= prompt("¿Posee cupón de descuento?");
+
+if(respueta2=="si"){
+    total = total*0.75;
+}
+
+let respueta3= prompt("¿Como desea abonar? 1-Efectivo  2-Tarjeta  3-Mercado Pago");
+
+switch(respueta3){
+    case "1":
+        alert("Eligio pago en efectivo");
+        alert("El total a pagar es: $" + parseInt(total));
+    break;
+
+    case "2":
+        alert("Eligio pago con tarjeta");
+        let cuotas=prompt("¿En cuantas cuotas lo abonara?");
+        switch(cuotas){
+            case "1":
+                alert("Total a pagar por mes: $" + parseInt(total));
+            break;
+            case "6":
+                total=(total*1.075)/6;
+                alert("Total a pagar por mes: $" + parseInt(total));
+            break;
+            case "12":
+                total=(total*1.14)/12;
+                alert("Total a pagar por mes: $" + parseInt(total));
+            break;
+        }
+    break;
+
+    case "3":
+        alert("Eligio pago con Mercado Pago");
+        alert("El total a pagar es: $" + parseInt(total));
+    break;
+
+    default:
+
+    break;
+}
+*/
